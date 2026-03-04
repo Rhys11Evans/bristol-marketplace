f867175dfd6982d66955d319a8bc23eee63c173a
Rhys Evans <Rhys11.evans@live.uwe.ac.uk>
2026-03-04 12:27:21 +0000

    Sprint 1: Product API and database models

--- products/views.py
+++ products/views.py
@@ -4,10 +4,6 @@ from .serializers import ProductSerializer
 
 
 class ProductListAPIView(generics.ListAPIView):
-    """
-    READ ONLY endpoint for Sprint 1:
-    - GET /api/products/ returns all products as JSON
-    """
 
     queryset = Product.objects.all().order_by("name")
     serializer_class = ProductSerializer
